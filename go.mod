972e7a7e2b8c0be43144c79f3d4e8f75ea11321a
longerwu <longerwu@tencent.com>
2023-05-30 22:11:15 +0800

    兼容各种编码

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/SnDragon/spider
 
 go 1.20
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/pkg/errors"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io"
 	"net/http"
 )
 
 func main() {
 	url := "https://www.thepaper.cn/"
-	rsp, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("fetch url err: %+v\n", err)
-		return
-	}
-	if rsp.StatusCode != http.StatusOK {
-		fmt.Printf("rsp status not 200\n")
-		return
-	}
-	bytes, err := io.ReadAll(rsp.Body)
+	bytes, err := Fetch(url)
 	if err != nil {
 		fmt.Printf("read body err: %+v\n", err)
 		return
 	}
-	fmt.Println("body: ", string(bytes))
+	fmt.Printf("rsp body: %v\n", string(bytes))
+}
+
+func Fetch(url string) ([]byte, error) {
+	rsp, err := http.Get(url)
+	if err != nil {
+		return nil, errors.Wrapf(err, "fetch url")
+	}
+	if rsp.StatusCode != http.StatusOK {
+		fmt.Printf("rsp status not 200\n")
+		return nil, errors.Errorf("rsp status: %v not 200", rsp.StatusCode)
+	}
+	reader := bufio.NewReader(rsp.Body)
+	e := DetermineEncoding(reader)
+	utf8Reader := transform.NewReader(reader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
+
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		fmt.Printf("DetermineEncoding Peek err: %+v", err)
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
